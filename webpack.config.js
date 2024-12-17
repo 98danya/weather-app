8ee5f600e6c636493ade867f805256fd78b0d038
98.danya <1998.danyamohammed@gmail.com>
2024-12-17 10:18:54 +0100

    Added missing Plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,25 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin"); // Add this line
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    watchFiles: ['./src/index.html'],
+    watchFiles: ["./src/index.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: 'styles.css',
+      filename: "styles.css", // Ensure this is defined correctly
     }),
   ],
   module: {
@@ -27,16 +28,16 @@ module.exports = {
         test: /\.css$/i,
         use: [
           MiniCssExtractPlugin.loader,
-          'css-loader',
+          "css-loader",
         ],
       },
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: "html-loader",
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
